0d5752dfdd7993cf3563a7ca1bf5591a815aaa0f
Adrian Jiga <jiga.ion.adrian@gmail.com>
2025-11-29 20:32:47 +0200

    remove unused Optional import from BaseUITest

--- src/test/java/com/example/tests/ui/BaseUITest.java
+++ src/test/java/com/example/tests/ui/BaseUITest.java
@@ -11,7 +11,6 @@ import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class BaseUITest {
     protected WebDriver driver;
